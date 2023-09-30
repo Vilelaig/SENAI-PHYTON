edb251d86f06915ae2dbf902f85e4fda35dbcec3
Vilelaig <146586399+Vilelaig@users.noreply.github.com>
2023-09-30 16:30:57 -0300

    Add files via upload

---
+++ aula 1.py
@@ -0,0 +1,86 @@
+# nome = input("digite aqui seu nome")
+# idade = input("digite aqui sua idade")
+# meu_endereco = input("digite seu endereco")
+# cargo = input('digite aqui seu cargo')
+# print('Meu nome é, ', nome , 'minha idade é, ' ,idade, 'meu endereço é, ', meu_endereco, 'meu cargo é, ' ,cargo)
+
+
+#  nome = input("digite aqui seu nome - ")
+#  idade = input("digite aqui sua idade - ")
+# #  meu_endereco = input("digite seu endereco")
+# #  cargo = input('digite aqui seu cargo')
+# print('meu nome é {nome}' , Minha idade é {idade})
+
+# frase = "gosto de bombom"
+# frase_2 =input("digite a frase chave")
+
+# print(f'A frase é {frase}, {frase_2}')
+
+
+# hh = input("coloque a hora")
+# mm = input("coloque o minuto")
+# ss = input("coloque os segundos")
+
+# print(f' {hh}, {mm}, {ss}')
+
+
+# number_1 =input(" Digite seu DDD: ")
+# number_2 = input(" Digite seu numero celular: ")
+
+# print(f'{number_1}, {number_2}')
+
+
+# item_1 =input("primeiro item")
+# item_2 =input("segundo item")
+# item_3 =input("terceiro item")g
+# item_4 =input("quarto item")
+
+# print(f' A sua lista tem, {item_1}, {item_2}, {item_3}, {item_4}') 
+
+
+# adj_1 =input("primeiro adj 1")
+# adj_2 =input("primeiro adj 2")
+# adj_3 =input("primeiro adj 3")
+
+# print(f'fiz um bolo {adj_1} que depois de um tempo achei {adj_2} mas depois vi que era {adj_3}')
+
+
+
+# num = float(input("Digite um numero"))
+# if num== 10:
+
+#  print('O numero é 10')
+# else:
+#  print(f' o numero é {num}')
+
+# senha_digitada = input("Digite uma senha")
+# nome = input("Digite seu nome")
+# senha = '1234'
+
+# if  senha == senha_digitada and nome == 'Camila':
+#     print("acesse, vc digitou corretamente")
+# else:
+#     print('Nao pode acesssar, porque vc digitou {senha_digitada} ou {nome} incorretamente')
+
+
+# idade = int(input ("digite sua idade para poder entrar no show:"))
+# if idade>= 18:
+#    print("liberado")
+# else:
+#    print("rejeitado")
+
+# idade = int(input("digite a sua idade"))
+# if idade >= 45:
+#     print("maior de idade")
+# if idade <= 18:
+#     print ("menor de idade")
+
+
+n1 = notas = float(input("digite a sua nota"))
+n2 = notas = float(input("digite a sua nota"))              
+n3 = notas = float(input("digite a sua nota"))
+media=(n1+n2+n3)/3
+if media >= 5:
+   print("aprovado")
+else:
+   print("reprovado")
